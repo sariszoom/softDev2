d025bbdd4df7f4cf0981237a3afc203be993ffe4
sariszoom <sariszoom@gmail.comuyuftytyddtydtyfyufyufyuftyudtdghdghdfdfgdfffhjftyffdrtdtysrtdtydtydfrtsddghfghsddghhjghfghjgfghjfghfghlgkl;jdfklgjfkldgjldfkjglkfjgh;lkdgjhlkjfgf;dkds;fkjhjihkdfkjfdigjiodfjgl;lkpso>
2023-01-02 21:50:54 +0700

    MongoDB

--- index.js
+++ index.js
@@ -1,18 +1,62 @@
 const express = require('express')
-const app = express()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
-app.get('/',(req,res) => {
-    res.send('welcome!')
+const dbConfig = require('./config/mongodb.config.js')
+const Customer = require('./models/customer.js')
+
+const cors = require('cors')
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}))
+
+mongoose.Promise = global.Promise;
+mongoose.connect(dbConfig.url)
+    .then(()=>{
+        Customer.deleteMany({},(err)=>{
+    if (err){
+        process.exit();
+            }
+            console.log('Remove Collection of Customer')
+            initCustomer();
+        });
+    }).catch(err=>{
+        console.log('Cannot Connect to MongoDB')
+        process.exit();
+    })
+
+app.use(cors())
+require('./routes/customer.route.js')(app);
+
+const server = app.listen(3000,()=>{
+    let port = server.address().port
+    console.log('Run at http://localhost:%s', port)
 })
 
-app.listen(3000, () => {
-    console.log("Start server at Port [3000]")
-})
-
-app.get('*',(req,res) => {
-    res.sendFile(__dirname + '/public/Error.html')
-})
-
-
-
-// continue framework2
+function initCustomer(){
+    let data = [
+        {
+            CustomerId: 1001,
+            FullName: "Krisada",
+            Address: "Nonthaburi"
+        },
+        {
+            CustomerId: 1002,
+            FullName: "Ton",
+            Address: "Bangkok"
+        },
+        {
+            CustomerId: 1003,
+            FullName: "Dream",
+            Address: "Chiangmai"
+        }
+    ]
+    for(let i=0; i<data.length; i++){
+        const c = new Customer(data[i]);
+        c.save()
+    }
+    console.log("สร้างข้อมูล Customer สำเร็จแล้ว")
+}
