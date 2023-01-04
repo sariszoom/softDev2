b518a2872a46f6a1f39d9f4dbf1603dd3361b734
sariszoom <sariszoom@gmail.comuyuftytyddtydtyfyufyufyuftyudtdghdghdfdfgdfffhjftyffdrtdtysrtdtydtydfrtsddghfghsddghhjghfghjgfghjfghfghlgkl;jdfklgjfkldgjldfkjglkfjgh;lkdgjhlkjfgf;dkds;fkjhjihkdfkjfdigjiodfjgl;lkpso>
2023-01-04 13:58:54 +0700

    class7

--- controllers/customer.controller.js
+++ controllers/customer.controller.js
@@ -14,3 +14,63 @@ exports.findAll = (req, res) => {
     })
 }
 
+exports.create = (req,res) => {
+    const c = new customer(req.body)
+
+    c.save().then(data => {
+        res.json(data)
+
+    }).catch(err => {
+        return res.status(500).json({
+            msg: "ไม่สามารถเพิ่มข้อมูลได้ เนื่องจาก : " + err.message
+        })
+    })
+}
+
+exports.findById = (req, res) =>{
+    customer.findById(req.params.customerId).then(data=>{
+        if(!data){
+            return res.status(404).json({
+                msg: "ไม่พบ Record รหัส : " + req.params.customerId
+            })
+        }
+        res.json(data)
+    }).catch(err => {
+        return res.status(500).json({
+            msg: "เกิดข้อผิดพลาด เนื่องจาก : " + err.message
+        })
+    })
+}
+
+exports.update = (req, res) => {
+    customer.findByIdAndUpdate(req.params.customerId, {$set: req.body}, {new: true})
+    .then(data => {
+        if(!data) {
+            return res.status(404).json({
+                msg: "ไม่พบ Record รหัส : " + req.params.customerId
+            })
+        }
+        res.json(data)
+    }).catch(err => {
+        return res.status(500).json({
+            msg: "ไม่สามารถ Update ข้อมูลได้ เนื่องจาก : " + err.message
+        })
+    })
+}
+
+exports.delete = (req, res) => {
+    customer.findByIdAndDelete(req.params.customerId, {$set: req.body}, {new: true})
+    .then(data => {
+        if(!data) {
+            return res.status(404).json({
+                msg: "ไม่พบ Record รหัส : " + req.params.customerId
+            })
+        }
+        res.json(data)
+    }).catch(err => {
+        return res.status(500).json({
+            msg: "ไม่สามารถ Update ข้อมูลได้ เนื่องจาก : " + err.message
+        })
+    })
+}
+
